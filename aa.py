1c97c5567fda53d6a96a4b3c4c4ff9aa3060ce2c
Sutha Binkamit <42909437+sutha001@users.noreply.github.com>
2018-12-15 18:57:16 +0700

    Update aa.py

--- aa.py
+++ aa.py
@@ -1,7 +1,30 @@
 import csv
-file51 = open("2551.txt")
-data51 = csv.reader(file51)
-lis = []
+data51 = csv.reader(open("2551.txt"))
+data52 = csv.reader(open("2552.txt"))
+data53 = csv.reader(open("2553.txt"))
+data54 = csv.reader(open("2554.txt"))
+data55 = csv.reader(open("2555.txt"))
+top10_2551 = []
+top10_2552 = []
+top10_2553 = []
+top10_2554 = []
+top10_2555 = []
 for i in data51:
-    lis.append(i)
-print(lis)
+    top10_2551.append(i)
+print(2551,top10_2551[:10])
+
+for i in data52:
+    top10_2552.append(i)
+print(2552,top10_2552[:10])
+
+for i in data53:
+    top10_2553.append(i)
+print(2553,top10_2553[:10])
+
+for i in data54:
+    top10_2554.append(i)
+print(2554,top10_2554[:10])
+
+for i in data55:
+    top10_2555.append(i)
+print(2555,top10_2555[:10])
