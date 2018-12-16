8e3504419c855807700c0445c6881eabe97d2328
Sutha Binkamit <42909437+sutha001@users.noreply.github.com>
2018-12-16 15:39:49 +0700

    Add files via upload

---
+++ top2551.py
@@ -0,0 +1,31 @@
+import csv
+import pygal
+data51 = csv.reader(open("2551.txt"))
+count = 0
+top10 = []
+for i in data51:
+    top10.append(i)
+top10_2551 = top10[:10]
+disease_name = []
+num = []
+for i in top10_2551:
+    disease_name.append(i[0])
+for i in top10_2551:
+    num.append(i[1])
+print(disease_name)
+print(num)
+
+line = pygal.Bar()
+line.title = "จำนวนผู้ป่วยปี2551 Top 10"
+line.x_labels = ("")
+line.add(disease_name[0], int(num[0]))
+line.add(disease_name[1], int(num[1]))
+line.add(disease_name[2], int(num[2]))
+line.add(disease_name[3], int(num[3]))
+line.add(disease_name[4], int(num[4]))
+line.add(disease_name[5], int(num[5]))
+line.add(disease_name[6], int(num[6]))
+line.add(disease_name[7], int(num[7]))
+line.add(disease_name[8], int(num[8]))
+line.add(disease_name[9], int(num[9]))
+line.render_to_file("top10_2551.svg")\
